40955f14b86e0cb1834fc7f161f33bdee8678615
TheCudConniseur <thecudconniseur@gmail.com>
2022-07-17 15:13:20 -0600

    Episode 14
    Episode 14 Commit

---
+++ PROJECT1/Assets/_Scripts/WeaponItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SG
+{
+    [CreateAssetMenu(menuName = "Items/Weapon Item")]
+    public class WeaponItem : Item
+    {
+        public GameObject modelPrefab;
+        public bool isUnarmed;
+
+        [Header("Idle Animations")]
+        public string right_hand_idle;
+        public string left_hand_idle;
+
+        [Header("Attack Animations")]
+        public string oh_light_attack_01;
+        public string oh_light_attack_02;
+    }
+}
